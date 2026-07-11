1d311cae08023b613efc33cf3e4876c474241103
H <hani1386reza31@gmail.com>
2026-07-11 19:37:45 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// بعد از دیپلوی کردن Code.gs در گوگل اپس اسکریپت (طبق راهنمای README.md)،
+// لینک Web App رو اینجا جایگزین کن.
+const APPS_SCRIPT_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
