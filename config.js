508db1a39502fbe8b24db872f62534972578901c
H <hani1386reza31@gmail.com>
2026-07-14 14:52:36 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // بعد از دیپلوی کردن Code.gs در گوگل اپس اسکریپت (طبق راهنمای README.md)،
 // لینک Web App رو اینجا جایگزین کن.
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzW68JGHrZUUD5zEhFqE6iF4zfEdNioUAzhfkupKHRudW4zyL92THCKJT4ftkBz6W2/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbypqhDKPslweQunWnTtqo2GrmTQdirSaUlJSPnG_ETEUwHwCbBAnjGb5JKIdTFIJccO/exec";
